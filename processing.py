c7f61203450b4abd4516ae7b7dd4aac3ecfa8f3a
minh5 <minh.v.mai@gmail.com>
2015-12-11 16:28:08 -0500

    parsed/cleaned text and times. dataframe working

--- processing.py
+++ processing.py
@@ -42,28 +42,6 @@ def getStopWordList(stopWordListFileName):
     fp.close()
     return stopWords
 
-
-def getFeatureVector(tweet):
-    """create a feature vector  for training"""
-
-    featureVector = []
-    #split tweet into words
-    words = tweet.split()
-    for w in words:
-        #replace two or more with two occurrences
-        w = replaceTwoOrMore(w)
-        #strip punctuation
-        w = w.strip('\'"?,.')
-        #check if the word stats with an alphabet
-        val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
-        #ignore if it is a stop word
-        if(w in stopWords or val is None):
-            continue
-        else:
-            featureVector.append(w.lower())
-    return featureVector
-
-
 def randomSample(x, n):
     """return a random sample of a dataframe"""
 
